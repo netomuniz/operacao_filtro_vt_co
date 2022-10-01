c78ad2d8073a0c53b0abf00dcedf9b0f0e3821eb
netomuniz <netomuniz@gmail.com>
2022-10-01 18:43:47 -0400

    Commit App Admin Cidade

--- cidade/admin.py
+++ cidade/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . models import Estado, Cidade, Area
 
-# Register your models here.
+
+@admin.register(Estado)
+class EstadoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Cidade)
+class CidadeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    pass
--- cidade/models.py
+++ cidade/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Estado(models.Model):
+    estado = models.CharField(max_length=100)
+    uf = models.CharField(max_length=2)
+
+    def __str__(self) -> str:
+        return self.estado
+
+
+class Cidade(models.Model):
+    uf = models.ForeignKey(Estado, on_delete=models.CASCADE)
+    cidade = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.cidade
+
+
+class Area(models.Model):
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE)
+    area = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.area
