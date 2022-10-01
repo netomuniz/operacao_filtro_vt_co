ee17ff74aaf7b66b6cb753756ad9ba6a995ef093
netomuniz <netomuniz@gmail.com>
2022-10-01 17:23:04 -0400

    Commit Inicial do projeto configurado

---
+++ atendimentos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AtendimentosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'atendimentos'
---
+++ cidade/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CidadeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cidade'
